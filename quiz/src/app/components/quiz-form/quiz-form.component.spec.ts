96ddf64ac59ada6281fbe2c4d15b6dfd93fcb177
Vesna S <vesnasdoboj@gmail.com>
2020-03-10 18:16:04 +0100

    Update quiz-form.component.spec.ts

--- quiz/src/app/components/quiz-form/quiz-form.component.spec.ts
+++ quiz/src/app/components/quiz-form/quiz-form.component.spec.ts
@@ -11,139 +11,150 @@ import { QuizzesService } from 'src/app/services/quizzes.service';
 import { QuizFormComponent } from './quiz-form.component';
 
 describe('QuizFormComponentTest', () => {
-	let fixture: ComponentFixture<QuizFormComponent>;
-	let component: QuizFormComponent;
-	let debugElement: DebugElement;
-	let quizzesService: QuizzesService;
-	let activatedRoute: ActivatedRoute;
-	let router: Router;
+    let fixture: ComponentFixture<QuizFormComponent>;
+    let component: QuizFormComponent;
+    let debugElement: DebugElement;
+    let quizzesService: QuizzesService;
+    let activatedRoute: ActivatedRoute;
+    let router: Router;
 
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			imports: [HttpClientTestingModule, FormsModule, RouterTestingModule],
-			declarations: [QuizFormComponent],
-			providers: [QuizzesService, {
-				provide: ActivatedRoute, useValue: {
-					snapshot: { params: { id: 1 } }
-				}
-			}]
-		}).compileComponents();
-	}));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, FormsModule, RouterTestingModule],
+            declarations: [QuizFormComponent],
+            providers: [QuizzesService, {
+                provide: ActivatedRoute, useValue: {
+                    snapshot: { params: { id: 1 } }
+                }
+            }]
+        }).compileComponents();
+    }));
 
-	beforeEach(() => {
-		fixture = TestBed.createComponent(QuizFormComponent);
-		component = fixture.componentInstance;
-		debugElement = fixture.debugElement;
-		quizzesService = TestBed.get(QuizzesService);
-		activatedRoute = TestBed.get(ActivatedRoute);
-		router = TestBed.get(Router);
-	});
+    beforeEach(() => {
+        fixture = TestBed.createComponent(QuizFormComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+        quizzesService = TestBed.get(QuizzesService);
+        activatedRoute = TestBed.get(ActivatedRoute);
+        router = TestBed.get(Router);
+    });
 
-	it('should create component', () => {
-		expect(component).toBeDefined();
-	});
+    afterEach(() => {
+        fixture = null;
+        component = null;
+        debugElement = null;
+        quizzesService = null;
+        activatedRoute = null;
+        router = null;
+    });
 
-	it('should show "Loading data. Please wait..."', () => {
-		fixture.detectChanges();
+    it('should create component', () => {
+        expect(component).toBeDefined();
+    });
 
-		const divElement: HTMLDivElement = debugElement.query(By.css('div')).nativeElement;
+    it('should show "Loading data. Please wait..."', () => {
+        fixture.detectChanges();
 
-		expect(divElement.textContent).toEqual('Loading data. Please wait...');
-	});
+        expect(debugElement.query(By.css('div')).nativeElement.textContent).toEqual('Loading data. Please wait...');
+    });
 
-	it('should show empty form', () => {
-		activatedRoute.snapshot.params = { id: 0 };
+    it('should show empty form', async(() => {
+        component.quiz = { id: null, name: null };
 
-		component.ngOnInit();
+        fixture.detectChanges();
 
-		fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
 
-		const nameElement: HTMLInputElement = debugElement.query(By.css('#name')).nativeElement;
-		const buttonElement: HTMLButtonElement = debugElement.query(By.css('button')).nativeElement;
+            expect(debugElement.query(By.css('#id')).nativeElement.value).toEqual('');
+            expect(debugElement.query(By.css('#name')).nativeElement.value).toEqual('');
+            expect(debugElement.query(By.css('button')).nativeElement.textContent).toEqual('Save');
+        });
 
-		expect(nameElement.value).toEqual('');
-		expect(buttonElement.textContent).toEqual('Save');
-	});
+    }));
 
-	it('should show form with quiz', async(() => {
-		const spy = spyOn(quizzesService, 'findQuizById').and.returnValue(of(new HttpResponse({ body: { id: 1, name: 'Quiz' } })));
+    it('should show form with quiz', async(() => {
+        component.quiz = { id: 1, name: 'Quiz' };
 
-		fixture.detectChanges();
+        fixture.detectChanges();
 
-		fixture.whenStable().then(() => {
-			fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
 
-			const nameElement: HTMLInputElement = debugElement.query(By.css('#name')).nativeElement;
-			const buttonElement: HTMLButtonElement = debugElement.query(By.css('button')).nativeElement;
+            expect(debugElement.query(By.css('#id')).nativeElement.value).toEqual('1');
+            expect(debugElement.query(By.css('#name')).nativeElement.value).toEqual('Quiz');
+            expect(debugElement.query(By.css('button')).nativeElement.textContent).toEqual('Update');
+        });
 
-			expect(nameElement.value).toEqual('Quiz');
-			expect(buttonElement.textContent).toEqual('Update');
-		});
+    }));
 
-		component.ngOnInit();
-	}));
+    it('should init quiz', async(() => {
+        const spy = spyOn(quizzesService, 'findQuizById').and.returnValue(of(new HttpResponse({ body: { id: 1, name: 'Quiz' } })));
 
-	it('should show error message', async(() => {
-		const spyQuizzesService = spyOn(quizzesService, 'findQuizById').and.returnValue(throwError({ error: { message: 'No quiz with id 1' } }));
-		const spyAlert = spyOn(window, 'alert');
-		const spyRouter = spyOn(router, 'navigateByUrl');
+        fixture.detectChanges();
 
-		fixture.whenStable().then(() => {
-			fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
 
-			expect(spyQuizzesService.calls.first().args[0]).toEqual(1);
-			expect(spyAlert.calls.first().args[0]).toEqual('No quiz with id 1');
-			expect(spyRouter.calls.first().args[0]).toEqual('/quizzes/form');
-		});
+            expect(spy).toHaveBeenCalledWith(1);
+            expect(component.quiz).toEqual({ id: 1, name: 'Quiz' });
+        });
 
-		component.ngOnInit();
-	}));
+        component.ngOnInit();
+    }));
 
-	it('should save quiz', async(() => {
-		const spyQuizzesService = spyOn(quizzesService, 'saveQuiz').and.returnValue(of(new HttpResponse({ body: { id: 1, name: 'Quiz' } })));
-		const spyAlert = spyOn(window, 'alert');
+    it('should show error message', async(() => {
+        const spy1 = spyOn(quizzesService, 'findQuizById').and.returnValue(throwError({ error: { message: 'No quiz with id 1' } }));
+        const spy2 = spyOn(window, 'alert');
+        const spy3 = spyOn(router, 'navigateByUrl');
 
-		component.quiz = { id: null, name: 'Quiz' };
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
 
-		fixture.detectChanges();
+            expect(spy1).toHaveBeenCalledWith(1);
+            expect(spy2).toHaveBeenCalledWith('No quiz with id 1');
+            expect(spy3).toHaveBeenCalledWith('/quizzes/form');
+        });
 
-		fixture.whenStable().then(() => {
-			fixture.detectChanges();
+        component.ngOnInit();
+    }));
 
-			expect(spyQuizzesService.calls.first().args[0]).toEqual({ id: null, name: 'Quiz' });
-			expect(spyAlert.calls.first().args[0]).toEqual('Your Data Has Been Successfully Saved.');
-			expect(component.quiz).toEqual({ id: 1, name: 'Quiz' });
-		});
+    it('should save quiz', async(() => {
+        component.quiz = { id: null, name: 'Quiz' };
 
-		component.saveQuiz();
-	}));
+        const spy1 = spyOn(quizzesService, 'saveQuiz').and.returnValue(of(new HttpResponse({ body: { id: 1, name: 'Quiz' } })));
+        const spy2 = spyOn(window, 'alert');
 
-	it('should update quiz', async(() => {
-		const spyQuizzesService = spyOn(quizzesService, 'updateQuiz').and.returnValue(of(new HttpResponse({ body: { id: 1, name: 'Quiz' } })));
-		const spyAlert = spyOn(window, 'alert');
+        fixture.detectChanges();
 
-		component.quiz = { id: 1, name: 'Quiz' };
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
 
-		fixture.detectChanges();
+            expect(spy1).toHaveBeenCalledWith({ id: null, name: 'Quiz' });
+            expect(spy2).toHaveBeenCalledWith('Your Data Has Been Successfully Saved.');
+            expect(component.quiz).toEqual({ id: 1, name: 'Quiz' });
+        });
 
-		fixture.whenStable().then(() => {
-			fixture.detectChanges();
+        component.saveQuiz();
+    }));
 
-			expect(spyQuizzesService.calls.first().args[0]).toEqual({ id: 1, name: 'Quiz' });
-			expect(spyAlert.calls.first().args[0]).toEqual('Your Data Has Been Successfully Updated.');
-			expect(component.quiz).toEqual({ id: 1, name: 'Quiz' });
-		});
+    it('should update quiz', async(() => {
+        component.quiz = { id: 1, name: 'Quiz' };
 
-		component.updateQuiz();
-	}));
+        const spy1 = spyOn(quizzesService, 'updateQuiz').and.returnValue(of(new HttpResponse({ body: { id: 1, name: 'Quiz' } })));
+        const spy2 = spyOn(window, 'alert');
 
-	afterEach(() => {
-		fixture = null;
-		component = null;
-		debugElement = null;
-		quizzesService = null;
-		activatedRoute = null;
-		router = null;
-	});
+        fixture.detectChanges();
+
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
+
+            expect(spy1).toHaveBeenCalledWith({ id: 1, name: 'Quiz' });
+            expect(spy2).toHaveBeenCalledWith('Your Data Has Been Successfully Updated.');
+            expect(component.quiz).toEqual({ id: 1, name: 'Quiz' });
+        });
+
+        component.updateQuiz();
+    }));
 
 });
